b92b9d39a28d1b6bc5ce42486544a5210b52ed09
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 05:24:37 +0000

    [automatic commit] 2025-06-21 05:24:37 UTC 110.141.212.45

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -11,7 +11,7 @@ router.get('/summary', async (req, res) => {
         Users.username AS walker_username,
         COUNT(DISTINCT WalkRatings.rating_id) AS total_ratings,
         AVG(WalkRatings.rating) as average_rating),
-        COUNT(DISTINCT CASE WHEN WalkRequests.status =)
+        COUNT(DISTINCT CASE WHEN WalkRequests.status = 'completed' THEN WalkRequests.request_id
       FROM Users LEFT JOIN WalkRatings ON WalkRatings.walker_id = Users.user_id
           LEFT JOIN WalkRequests ON WalkRequest.request_id = WalkRatings.request_id AND WalkRequest.status = 'completed'
       WHERE Users.role = 'walker'
