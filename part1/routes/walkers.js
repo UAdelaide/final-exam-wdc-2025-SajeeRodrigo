25bceafad9dce90962f3dd05ace70b9e97eb96b6
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 05:26:29 +0000

    [automatic commit] 2025-06-21 05:26:29 UTC 110.141.212.45

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -8,15 +8,17 @@ router.get('/summary', async (req, res) => {
     const db = await getConnection();
     const [result] = await db.query(`
       SELECT
-        Users.username AS walker_username,
-        COUNT(DISTINCT WalkRatings.rating_id) AS total_ratings,
-        AVG(WalkRatings.rating) as average_rating),
-        COUNT(DISTINCT CASE WHEN WalkRequests.status = 'completed' THEN WalkRequests.request_id END)
-      FROM Users
-        LEFT JOIN WalkRatings ON WalkRatings.walker_id = Users.user_id
-        LEFT JOIN WalkRequests ON WalkRequest.request_id = WalkRatings.request_id AND WalkRequest.status = 'completed'
-      WHERE Users.role = 'walker'
-      GROUP BY user_id, username
+        Users.username,
+  COUNT(DISTINCT WalkRatings.rating_id) ,
+  ROUND(AVG(WalkRatings.rating), 2),
+  COUNT(DISTINCT CASE WHEN WalkRequests.status = 'completed' THEN WalkRequests.request_id END)
+  FROM Users
+    LEFT JOIN WalkRatings ON WalkRatings.walker_id = Users.user_id
+    LEFT JOIN WalkRequests ON WalkRequests.request_id = WalkRatings.request_id AND WalkRequests.status = 'completed'
+  WHERE Users.role = 'walker'
+  GROUP BY Users.user_id, Users.username
+  ORDER BY Users.username ASC;
+
       ;
     `);
     res.json(walkRequest);
