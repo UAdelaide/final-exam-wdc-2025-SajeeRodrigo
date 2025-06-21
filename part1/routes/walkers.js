f5b4e0f98d3b70877c9bc38a45a84f1aeebfbedd
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 05:17:48 +0000

    [automatic commit] 2025-06-21 05:17:48 UTC 110.141.212.45

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -11,6 +11,7 @@ router.get('/summary', async (req, res) => {
       FROM Users LEFT JOIN WalkRatings ON WalkRatings.walker_id = Users.user_id
           LEFT JOIN WalkRequests ON WalkRequest.request_id = WalkRatings.request_id AND WalkRequest.status = 'completed'
       WHERE Users.role = 'walker'
+      
       ;
     `);
     res.json(walkRequest);
