cf6750457b2a60ff1e74c79c5586f208db46b5fd
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 04:14:42 +0000

    [automatic commit] 2025-06-21 04:14:41 UTC 110.141.212.45

--- part1/db.js
+++ part1/db.js
@@ -1,3 +1,16 @@
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+  socketPath: '/var/run/mysqld/mysqld.sock',
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'newpassword',
+  database: 'textbook_marketplace'
+});
+
+module.exports = db;
+
+
 const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
