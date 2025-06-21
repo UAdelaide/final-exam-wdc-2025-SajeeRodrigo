77a29cbb40b7826fddd849f7f323baefb20853f0
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 05:00:45 +0000

    [automatic commit] 2025-06-21 05:00:44 UTC 110.141.212.45

--- part1/routes/walkRequest.js
+++ part1/routes/walkRequest.js
@@ -5,12 +5,12 @@ const {getConnection} = require('../db');
 /* GET users listing. */
 router.get('/:status', async (req, res) => {
   try {
-    let {status} = req.query;
+    let {status} = req.params;
     const db = await getConnection();
     const [walkRequest] = await db.query(`
       SELECT request_id, Dogs.name as dog_name, requested_time, duration_minutes, location, Users.username as owner_username
       FROM ((WalkRequests INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id) INNER JOIN Users ON Dogs.owner_id = Users.user_id)
-      WHERE status == ;
+      WHERE status == ?;
     `);
     res.json(walkRequest);
   } catch (err) {
