aebe6158c6da6d3a9ad7bb06460c5d25516131e4
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 05:11:59 +0000

    [automatic commit] 2025-06-21 05:11:58 UTC 110.141.212.45

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -7,7 +7,7 @@ router.get('/summary', async (req, res) => {
   try {
     const db = await getConnection();
     const [result] = await db.query(`
-      SELECT Users.username AS walker_username, COUNT(DISTINCT WalkRequests.ratings_id) AS;
+      SELECT Users.username AS walker_username, COUNT(DISTINCT WalkRequests.ratings_id) AS total_ratings, ;
     `);
     res.json(walkRequest);
   } catch (err) {
