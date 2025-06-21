0505f72c2fd8342d93f546c15a9afa2a489ac49c
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 04:09:19 +0000

    [automatic commit] 2025-06-21 04:09:19 UTC 110.141.212.45

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', async (req, res) => {
   const [rows] = await db.query(`
-    SELECT 
+    SELECT name as dog_name, 
     FROM
 
   `);
