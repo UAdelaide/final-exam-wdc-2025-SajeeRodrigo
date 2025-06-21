1be0bbc93e56a116cc30c67d0b81be445e76cb0b
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 04:03:52 +0000

    [automatic commit] 2025-06-21 04:03:52 UTC 110.141.212.45

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/items', async (req, res) => {
+  const [rows] = await db.query(`
+    SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
+    FROM BookListings bl
+    JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
+    JOIN Users u ON bl.SellerID = u.UserID
+  `);
+  res.json(rows);
 });
-
 module.exports = router;
