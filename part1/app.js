23ea68d51e46fd0968cc9fe58f2889f89a73b528
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 04:16:56 +0000

    [automatic commit] 2025-06-21 04:16:56 UTC 110.141.212.45

--- part1/app.js
+++ part1/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let db
+let db = require('./db.js')
 
 (async () => {
   try {
