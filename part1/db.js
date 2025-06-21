2f5c42a1efbdc4f18d11982266c5b3988fb1af0d
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 04:16:13 +0000

    [automatic commit] 2025-06-21 04:16:13 UTC 110.141.212.45

--- part1/db.js
+++ part1/db.js
@@ -5,23 +5,7 @@ const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
   password: '',
-  database: 'textbook_marketplace'
+  database: 'DogWalkService'
 });
 
 module.exports = db;
-
-
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '' // Set your MySQL root password
-});
-
-
-// Now connect to the created database
-db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'testdb'
-});
