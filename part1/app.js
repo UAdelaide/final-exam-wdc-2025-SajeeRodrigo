332359a1c07bc446dc3f1c56a7546ea1f00ff28e
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 02:52:02 +0000

    [automatic commit] 2025-06-21 02:52:02 UTC 110.141.212.45

--- part1/app.js
+++ part1/app.js
@@ -16,6 +16,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let db;
+
+(async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: ''
+    });
+
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+    await connection.end();
+
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'DogWalkService',
+      multipleStatements: true
+    });
+
+    const sqlFilePath = path.join(__dirname, 'dogwalks.sql');
+    const schemaSQL = fs.readFileSync(sqlFilePath, 'utf8');
+    await db.query(schemaSQL);
+
+    console.log('Database and tables setup complete.');
+  } catch (err) {
+    console.error('Error setting up database. Ensure MySQL is running:', err.message);
+  }
+})()
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
