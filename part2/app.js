87d4c27e8516dd1981f40da243469199f01b38c7
SajeeRodrigo <sajee.thamanga@gmail.com>
2025-06-21 06:58:38 +0000

    [automatic commit] 2025-06-21 06:58:38 UTC ;; communications error to 208.67.222.222#53: timed out 110.141.212.45

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-    secret : 'dff'
+    secret : 'SR!@#$'
 }))
 
 // Routes
